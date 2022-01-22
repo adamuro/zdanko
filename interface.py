8521b40acd42762abd4acca9cb85e4707354505f
Adam Turowski <horosporo@gmail.com>
2022-01-23 00:53:56 +0100

    Modify interface class to use courses wrappers

--- interface.py
+++ interface.py
@@ -1,44 +1,54 @@
-import cmd
-
-from scraper import Scraper
+from cmd import Cmd
+from courses import CoursesList, CoursesRepository
 from requirements import Requirements
 
-BASE_URL = 'https://zapisy.ii.uni.wroc.pl/courses'
-courses = Scraper(BASE_URL).courses
-requirements = Requirements()
-
-class Interface(cmd.Cmd):
-  intro = 'Witaj! Wpisz ? lub help, aby zobaczyć dostępne komendy.\n'
+class Interface(Cmd):
+  intro = 'Witaj! Wpisz help lub ?, aby zobaczyć dostępne komendy.\n'
+  doc_header = 'Dostępne komendy (wpisz help <komenda>):'
   prompt = '> '
+
+  def __init__(self, requirements: Requirements, finished_courses: CoursesList=CoursesList(), repository: CoursesRepository=CoursesRepository()):
+    super().__init__()
+    self.requirements = requirements
+    self.finished_courses = finished_courses
+    self.repository = repository
+    self.courses = CoursesList(repository.find_all()) # Because reading courses directly from the repository every time is too slow
 
   def do_add(self, arg):
     'Dodaj kurs do listy ukończonych kursów: add <nazwa kursu> <semestr: lato|zima> <rok: 2016/17|2017/18|...>'
-    matches = [course for course in courses if course['name'] == arg]
-    if len(matches) == 0:
+    course = self.courses.name(arg)
+    if not course:
       print('Nie znaleziono kursu o nazwie "' + arg + '". Spróbuj skorzystać z auto-uzupełniania pod tabem :^)')
     else:
-      requirements.add_course(matches[0])
+      self.finished_courses.add(course)
       print('Kurs o nazwie "' + arg + '" został dodany do listy!')
 
   def do_rm(self, arg):
     'Usuń kurs z listy ukończonych kursów: rm <nazwa kursu> <semestr: lato|zima> <rok: 2016/17|2017/18|...>'
-    matches = [course for course in requirements.courses if course['name'] == arg]
-    if len(matches) == 0:
+    course = self.finished_courses.name(arg)
+    if not course:
       print('Nie znaleziono kursu o nazwie "' + arg + '" na liście ukończonych kursów.\nSpróbuj skorzystać z auto-uzupełniania pod tabem :^)')
     else:
-      requirements.remove_course(matches[0])
+      self.finished_courses.remove(course)
       print('Kurs o nazwie "' + arg + '" został usunięty z listy!')
   
   def do_ls(self, arg):
     'Wypisz wszystkie kursy z listy ukończonych kursów'
-    print([course['name'] for course in requirements.courses])
+    print(self.finished_courses.names())
   
   def do_calc(self, arg):
     'Oblicz, czego jeszcze brakuje ci do osiągnięcia wymaganych efektów kształcenia'
-    print(requirements.check_engineer()['types'])
-    print(requirements.check_engineer()['effects'])
+    types_lacks, effects_lacks = self.requirements.check_engineer(self.finished_courses.all())
+    print(types_lacks)
+    print(effects_lacks)
+  
+  def do_update(self, arg):
+    'Uaktualnij bazę danych z kursami'
+    if self.repository.update():
+      self.courses = self.repository.find_all()
 
   def do_q(self, arg):
+    'Zamknij program'
     print('Do zobaczenia!')
     exit()
 
@@ -46,15 +56,18 @@ class Interface(cmd.Cmd):
     # Cmd autocompletes only the last word so we need to skip all the words before (offset)
     arg = line.partition(' ')[2]
     offset = len(arg) - len(text)
-    return [c['name'][offset:] for c in courses if c['name'].startswith(arg)]
+    return [name[offset:] for name in self.courses.names() if name.startswith(arg)]
 
   def complete_rm(self, text, line, begidx, endidx):
     # Cmd autocompletes only the last word so we need to skip all the words before (offset)
     arg = line.partition(' ')[2]
     offset = len(arg) - len(text)
-    return [c['name'][offset:] for c in requirements.courses if c['name'].startswith(arg)]
+    return [name[offset:] for name in self.finished_courses.names() if name.startswith(arg)]
 
   def precmd(self, line):
     return line.lower()
 
-Interface().cmdloop()
+  def emptyline(self):
+    return
+
+Interface(Requirements()).cmdloop()
